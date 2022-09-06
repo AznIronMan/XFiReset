3c64cb79d7749b5b7b9f09884924b0a3be4f974d
Geoff Clark <geoff@clarktribegames.com>
2022-09-05 19:33:44 -0700

    20220905-1932
    -removed speed-test npm due to it not working anymore, replaced with fast-cli npm
    -added logging file (optional)
    -modified outputs of speedresults for fast-cli instead of speed-test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-//Version 1.03 - 2022.08.18
+//Version 1.04 - 2022.08.18
 
 const pt = require("puppeteer");
 const fs = require("fs-extra");
 const smtp = require("nodemailer");
+const logger = require("node-logger").createLogger(
+  `${dateMaker(new Date())}.log`
+);
+logger.format = function (level, date, message) {
+  return `[${level} ${date.getHours().toString()}:${date
+    .getMinutes()
+    .toString()}:${date.getSeconds().toString()}]${message}`;
+};
 const exec = require("child_process").exec;
 let admin,
   transport,
@@ -13,11 +21,15 @@ let admin,
 
 async function run() {
   await adminCheck(); //checks for admin.json and speed-test npm requirement
+  lg("Starting Run...");
   if (admin.smtp.mode) {
+    lg("Building SMTP transport.");
     await buildTransport(); //if smtp is enabled, builds the smtp transporter
+    lg("SMTP transport has been built.");
   }
   needTest = await speedTest(); //runs speed test to see if the reboots are needed
   if (needTest) {
+    lg(`Starting Virtual Browser (Headless: ${admin.show}`);
     pt.launch({
       headless: admin.show,
       args: [
@@ -27,6 +39,8 @@ async function run() {
     }).then(async (browser) => {
       await adminCheck(); //check for admin.json file
       const p = await browser.newPage();
+      lg(`XFi:  ${admin.xfi.mode}`);
+      lg(`Netgear:  ${admin.netgear.mode}`);
       if (admin.xfi.mode) {
         await xfiReboot(p);
         await browser.close();
@@ -49,14 +63,19 @@ async function run() {
 
 async function speedTest() {
   if (admin.speed.mode) {
-    const speedResults = await runCmd("speed-test -j");
+    lg("Running Speed Test...");
+    const speedResults = await runCmd("fast --upload --json");
     const splitResults = speedResults.split(":");
-    const ping = parseInt(splitResults[1].split(",")[0]);
-    const down = parseInt(splitResults[2].split(",")[0]);
-    const up = parseInt(splitResults[3].split("}")[0]);
+    const ping = parseInt(splitResults[5].split(",")[0]);
+    const down = parseInt(splitResults[1].split(",")[0]);
+    const up = parseInt(splitResults[2].split("}")[0]);
     const pingResult = ping > admin.speed.ping;
     const downResult = down < admin.speed.down;
     const upResult = up < admin.speed.up;
+    lg("Speed Test Complete...");
+    lg(
+      `Ping: ${ping} Down: ${down} Up: ${up} Results: [${pingResult},${downResult},${upResult}]`
+    );
     let decision = false;
     speeds = `P:${ping}  D:${down}  U:${up}`;
     if (!decision && pingResult) {
@@ -69,19 +88,22 @@ async function speedTest() {
       decision = true;
     }
     if (decision) {
+      lg("Result show that SpeedTest is Required. #True");
       return true;
     } else {
+      lg("Result show that SpeedTest is NOT Required. #False");
       result("Speed Test", false);
     }
   } else {
+    lg("Speed Test Check Disabled, Reboots are required!");
     return true;
   }
 }
 
 async function adminCheck() {
   const npmList = await runCmd("npm list -g");
-  if (!npmList.includes("speed-test@")) {
-    await runCmd("npm install speed-test -g");
+  if (!npmList.includes("fast-cli@")) {
+    await runCmd("npm install fast-cli -g");
   }
   if (!fs.existsSync("./admin.json")) {
     await fs.writeFile("admin.json", fs.readFileSync("admin-template.json"));
@@ -89,12 +111,16 @@ async function adminCheck() {
   } else {
     admin = require("../admin.json");
     if (admin.xfi.pass == "password" && admin.netgear.pass == "password") {
+      lg(
+        "admin.json is still in raw format, please update admin.json to continue!"
+      );
       await fs.writeFile(
         "__FIX YOUR ADMIN.JSON__.txt",
         "Your admin.json is still the default settings, please update!"
       );
     } else {
       if (fs.existsSync("__FIX YOUR ADMIN.JSON__.txt")) {
+        lg("Removing fix admin.json reminder since it has been configured.");
         fs.rm("__FIX YOUR ADMIN.JSON__.txt", { force: true });
       }
     }
@@ -102,6 +128,7 @@ async function adminCheck() {
 }
 
 async function xfiLogin(page) {
+  lg("XFi Login Start.");
   await page.setViewport({ width: 1000, height: 500 });
   await page.goto(`http://${admin.xfi.ip}`);
   await page.click("#username");
@@ -109,9 +136,11 @@ async function xfiLogin(page) {
   await page.click("#password");
   await page.keyboard.type(admin.xfi.pass);
   await page.click("#pageForm > div.form-btn > input");
+  lg("XFi Login End.");
 }
 
 async function xfiReboot(page) {
+  lg("XFi Reboot Start.");
   await xfiLogin(page);
   await page.waitForSelector("#internet-usage > div > a", { visible: true });
   await page.goto(`http://${admin.xfi.ip}/restore_reboot.jst`);
@@ -119,10 +148,13 @@ async function xfiReboot(page) {
   await page.click("#btn1");
   await page.waitForTimeout(500);
   await page.click("#popup_ok");
+  lg("XFi Reboot Waiting...");
   await page.waitForTimeout(50000);
+  lg("XFi Reboot End.");
 }
 
 async function xfiResult() {
+  lg("XFi Result Start");
   try {
     pt.launch({ headless: false }).then(async (browser) => {
       const p = await browser.newPage();
@@ -149,23 +181,28 @@ async function xfiResult() {
       }
       await result("XFi Gateway Reboot", xfiTask);
       await browser.close();
+      lg("XFi Result End - Success");
     });
-  } catch {
+  } catch (ex) {
     xfiTask = false;
     await result("XFi Gateway Reboot", xfiTask);
+    lg(`XFi Result End - Exception: ${ex}`);
   }
 }
 
 async function netgearLogin(page) {
+  lg("Netgear Login Start.");
   await page.setViewport({ width: 1000, height: 500 });
   await page.authenticate({
     username: admin.netgear.user,
     password: admin.netgear.pass,
   });
   await page.goto(`http://${admin.netgear.ip}/adv_index.htm`);
+  lg("Netgear Login End.");
 }
 
 async function netgearReboot(page) {
+  lg("Netgear Reboot Start.");
   await netgearLogin(page);
   const frame = await page
     .frames()
@@ -196,10 +233,12 @@ async function netgearResult() {
       }
       await result("Netgear Router Reboot", netgearTask);
       await browser.close();
+      lg("Netgear Reboot End - Success.");
     });
-  } catch {
+  } catch (ex) {
     netgearTask = false;
     await result("Netgear Router Reboot", netgearTask);
+    lg(`Netgear Reboot End - Exception ${ex}`);
   }
 }
 
@@ -216,33 +255,35 @@ async function buildTransport() {
 }
 
 async function result(type, task) {
+  lg(`Results for ${type}...`);
+  const today = new Date();
+  let title, theResult;
+  title = "FAILURE";
+  if (admin.speed.mode) {
+    theResult = `failed!\n\nSpeed Test Results Before Test: ${speeds}`;
+  } else {
+    theResult = "failed!";
+  }
+  if (type.includes("Reboot")) {
+    if (task) {
+      if (admin.speed.mode) {
+        theResult = `was successful!\n\nSpeed Test Results Before Test: ${speeds}`;
+      } else {
+        theResult = "was successful!";
+      }
+      title = "SUCCESS";
+    }
+  } else {
+    if (!task) {
+      theResult = `${speeds
+        .replace("P:", "Ping: ")
+        .replace("D:", "Download: ")
+        .replace("U:", "Upload: ")}\n\nNo Reboots Were Needed!`;
+      title = `NO ACTION - ${speeds}`;
+    }
+  }
+  lg(theResult);
   if (admin.smtp.mode) {
-    const today = new Date();
-    let title, theResult;
-    title = "FAILURE";
-    if (admin.speed.mode) {
-      theResult = `failed!\n\nSpeed Test Results Before Test: ${speeds}`;
-    } else {
-      theResult = "failed!";
-    }
-    if (type.includes("Reboot")) {
-      if (task) {
-        if (admin.speed.mode) {
-          theResult = `was successful!\n\nSpeed Test Results Before Test: ${speeds}`;
-        } else {
-          theResult = "was successful!";
-        }
-        title = "SUCCESS";
-      }
-    } else {
-      if (!task) {
-        theResult = `${speeds
-          .replace("P:", "Ping: ")
-          .replace("D:", "Download: ")
-          .replace("U:", "Upload: ")}\n\nNo Reboots Were Needed!`;
-        title = `NO ACTION - ${speeds}`;
-      }
-    }
     let info = await transport.sendMail({
       from: admin.smtp.user,
       to: admin.smtp.to,
@@ -263,6 +304,23 @@ async function runCmd(cmd) {
       resolve(result);
     });
   });
+}
+
+function dateMaker(rawDate) {
+  const date = ("0" + rawDate.getDate()).slice(-2);
+  const month = ("0" + (rawDate.getMonth() + 1)).slice(-2);
+  const year = rawDate.getFullYear();
+  const hours = rawDate.getHours();
+  const minutes = rawDate.getMinutes();
+  const seconds = rawDate.getSeconds();
+  console.log(`${year}${month}${date}_${hours}${minutes}${seconds}`);
+  return `${year}${month}${date}_${hours}${minutes}${seconds}`;
+}
+
+function lg(loginfo) {
+  if (admin.log) {
+    logger.info(loginfo);
+  }
 }
 
 run();
